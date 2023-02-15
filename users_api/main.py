56750965e34bf642f559fa61d58e1d7f49fc4d8a
Mau Trigo <mtrigo143@gmail.com>
2023-02-14 20:31:22 -0400

    enable cors

--- users_api/main.py
+++ users_api/main.py
@@ -1,9 +1,18 @@
 from users_api.api.users.infra.users_routes import router as users_router
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 def create_server_app() -> FastAPI:
     app = FastAPI()
+
+    origins = ["*"]
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     app.include_router(users_router)
 
